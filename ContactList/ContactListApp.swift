d3405f4c9ea237873f999ce74551b66691e81a5a
Art <artem_chernenko@ro.ru>
2022-03-01 11:37:26 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Артем Черненко on 01.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
